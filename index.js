f1cc2fca68e658d05094b2d3ec79e6019848f996
Kevin Nitro <86353526+KevinNitroG@users.noreply.github.com>
2021-11-29 15:25:50 +0700

    :3
    :3

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-<div class="iframe">
-   <iframe sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" width="1000" height="600" src="https://diasfem.com/p/3rdgximqjqrze7x" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
-</div>
+
+window.onload = function() {
+    var button = document.getElementsByName("sandbox")[0]
+    var iframe = document.getElementsByName("framez")[0]
+    button.addEventListener('click',sndbx,false);
+
+    function sndbx(){
+    var nibba = document.getElementById("framez").src;
+    if(iframe.sandbox == 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation'){
+    document.getElementById("framez").removeAttribute("sandbox"); 
+    }
+    frames['framez'].location.href=nibba;
+    iframe.sandbox = 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation';
+    }
+} 
